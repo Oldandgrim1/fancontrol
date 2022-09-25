82fcc04e95ba2f5536feb8c8d312f20c485d458b
Oldandgrim1 <grim@darkunderworld.org>
2022-09-24 21:11:09 -0700

    Added fantest.py

---
+++ fantest.py
@@ -0,0 +1,13 @@
+#import the GPIO and time package
+import RPi.GPIO as io
+import time
+
+io.setmode(io.BCM)
+io.setup(18, io.OUT)
+while True:
+    io.output(18,1)
+    time.sleep(4)
+    io.output(18,0)
+    time.sleep(1)
+  
+
